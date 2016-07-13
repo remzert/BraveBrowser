893511d2a5651612e1c6e0003f052a44978ab2a7
Serg <serg.zhukovsky@gmail.com>
2016-07-13 10:35:42 -0400

    AdBlock, tracking protection, h264, aac support
    Auditors: @bbondy
    
    Conflicts:
    	chrome/android/BUILD.gn
    	out/Default/obj/chrome/android/chrome_public_apk_assets__build_config.ninja
    	out/Default/obj/chrome/android/chrome_public_apk_manifest.ninja
    	out/Default/obj/chrome/android/chrome_public_apk_resources__build_config.ninja
    	out/Default/obj/chrome/android/chrome_public_apk_resources__process_resources.ninja
    	out/Default/obj/chrome/android/chrome_public_apk_template_resources__template.ninja
    	out/Default/obj/chrome/android/chrome_public_test_apk_manifest.ninja
    	out/Default/obj/chrome/browser/browser.ninja
    
    Conflicts:
    	chrome/android/chrome_apk.gyp
    	chrome/browser/net/chrome_network_delegate.cc
    	chrome/chrome_android_paks.gypi
    	chrome/chrome_browser.gypi
    	media/base/key_systems.cc

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PrefServiceBridge.java
@@ -562,6 +562,20 @@ public final class PrefServiceBridge {
     }
 
     /**
+     * @param whether Tracking Protection should be enabled.
+     */
+    public void setTrackingProtectionEnabled(boolean enabled) {
+        nativeSetTrackingProtectionEnabled(enabled);
+    }
+
+    /**
+     * @param whether AdBlock should be enabled.
+     */
+    public void setAdBlockEnabled(boolean enabled) {
+        nativeSetAdBlockEnabled(enabled);
+    }
+
+    /**
      * @return whether Safe Browsing is managed
      */
     public boolean isSafeBrowsingManaged() {
@@ -1143,6 +1157,8 @@ public final class PrefServiceBridge {
     private native boolean nativeGetSafeBrowsingExtendedReportingManaged();
     private native boolean nativeGetSafeBrowsingEnabled();
     private native void nativeSetSafeBrowsingEnabled(boolean enabled);
+    private native void nativeSetTrackingProtectionEnabled(boolean enabled);
+    private native void nativeSetAdBlockEnabled(boolean enabled);
     private native boolean nativeGetSafeBrowsingManaged();
     private native boolean nativeGetNetworkPredictionManaged();
     private native boolean nativeObsoleteNetworkPredictionOptionsHasUserSetting();
--- media/base/key_systems.cc
+++ media/base/key_systems.cc
@@ -41,10 +41,8 @@ struct NamedCodec {
 static const NamedCodec kMimeTypeToCodecMasks[] = {
     {"audio/webm", EME_CODEC_WEBM_AUDIO_ALL},
     {"video/webm", EME_CODEC_WEBM_VIDEO_ALL},
-#if defined(USE_PROPRIETARY_CODECS)
     {"audio/mp4", EME_CODEC_MP4_AUDIO_ALL},
     {"video/mp4", EME_CODEC_MP4_VIDEO_ALL}
-#endif  // defined(USE_PROPRIETARY_CODECS)
 };
 
 // Mapping between codec names and enum values.
@@ -55,7 +53,6 @@ static const NamedCodec kCodecStrings[] = {
     {"vp8.0", EME_CODEC_WEBM_VP8},      // VP8.
     {"vp9", EME_CODEC_WEBM_VP9},        // VP9.
     {"vp9.0", EME_CODEC_WEBM_VP9},      // VP9.
-#if defined(USE_PROPRIETARY_CODECS)
     {"vp09", EME_CODEC_MP4_VP9},   // VP9 in MP4.
     {"mp4a", EME_CODEC_MP4_AAC},   // AAC.
     {"avc1", EME_CODEC_MP4_AVC1},  // AVC1.
@@ -64,7 +61,6 @@ static const NamedCodec kCodecStrings[] = {
     {"hev1", EME_CODEC_MP4_HEVC},  // HEV1.
     {"hvc1", EME_CODEC_MP4_HEVC},  // HVC1.
 #endif
-#endif  // defined(USE_PROPRIETARY_CODECS)
 };
 
 class ClearKeyProperties : public KeySystemProperties {
@@ -72,10 +68,8 @@ class ClearKeyProperties : public KeySystemProperties {
   std::string GetKeySystemName() const override { return kClearKeyKeySystem; }
 
   bool IsSupportedInitDataType(EmeInitDataType init_data_type) const override {
-#if defined(USE_PROPRIETARY_CODECS)
     if (init_data_type == EmeInitDataType::CENC)
       return true;
-#endif
     return init_data_type == EmeInitDataType::WEBM ||
            init_data_type == EmeInitDataType::KEYIDS;
   }
@@ -86,9 +80,7 @@ class ClearKeyProperties : public KeySystemProperties {
     // VP9 support is device dependent.
     SupportedCodecs codecs = EME_CODEC_WEBM_ALL;
 
-#if defined(USE_PROPRIETARY_CODECS)
     codecs |= EME_CODEC_MP4_ALL;
-#endif  // defined(USE_PROPRIETARY_CODECS)
 
     return codecs;
   }
