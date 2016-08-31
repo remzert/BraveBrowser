d3588ed2926bc43aeb0d9ef7e83a1ae337c79fe1
Serg <serg.zhukovsky@gmail.com>
2016-08-31 13:49:38 -0400

    Added per blockers switches
    Auditors: @bbondy

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -270,12 +270,24 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
   }
   // Ad Block and tracking protection
   bool isGlobalBlockEnabled = true;
+  bool blockAdsAndTracking = true;
+  bool isHTTPSEEnabled = true;
   net::blockers::ShieldsConfig* shieldsConfig =
     net::blockers::ShieldsConfig::getShieldsConfig();
   if (request && nullptr != shieldsConfig) {
       std::string hostConfig = shieldsConfig->getHostSettings(firstparty_host);
-      if ("0" == hostConfig) {
-          isGlobalBlockEnabled = false;
+      if (hostConfig.length() == 5) {
+        if ('0' == hostConfig[0]) {
+            isGlobalBlockEnabled = false;
+        }
+        if (isGlobalBlockEnabled) {
+            if ('0' ==  hostConfig[2]) {
+                blockAdsAndTracking = false;
+            }
+            if ('0' ==  hostConfig[4]) {
+                isHTTPSEEnabled = false;
+            }
+        }
       }
   }
   bool isTPEnabled = true;
@@ -287,6 +299,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
   int httpsUpgrades = 0;
 	if (request
       && isGlobalBlockEnabled
+      && blockAdsAndTracking
       && isTPEnabled
 			&& blockers_worker_.shouldTPBlockUrl(
 					firstparty_host,
@@ -302,6 +315,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 	const ResourceRequestInfo* info = ResourceRequestInfo::ForRequest(request);
 	if (!block
       && isGlobalBlockEnabled
+      && blockAdsAndTracking
       && isAdBlockEnabled
       && request
       && info
@@ -326,6 +340,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 
   // HTTPSE work
   if (isGlobalBlockEnabled
+      && isHTTPSEEnabled
       && check_httpse_redirect
       && enable_httpse_
       && enable_httpse_->GetValue()) {
