0a36c09254518724c28240ebd29cb2ef76d8bd65
Serg <serg.zhukovsky@gmail.com>
2016-09-20 13:29:55 -0400

    Fixed a crash on invalid URLs
    Auditors: @bbondy

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -408,14 +408,25 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
         }
       }
   }
+  bool isValidUrl = request->url().is_valid();
+  if (request) {
+      std::string scheme = request->url().scheme();
+      if (scheme.length()) {
+          std::transform(scheme.begin(), scheme.end(), scheme.begin(), ::tolower);
+          if ("http" != scheme && "https" != scheme) {
+              isValidUrl = false;
+          }
+      }
+  }
   bool isTPEnabled = true;
 	bool block = false;
   if (enable_tracking_protection_ && !shieldsSetExplicitly) {
     isTPEnabled = enable_tracking_protection_->GetValue();
   }
-  int adsAndTrakersBlocked = 0;
+  int adsAndTrackersBlocked = 0;
   int httpsUpgrades = 0;
 	if (request
+      && isValidUrl
       && isGlobalBlockEnabled
       && blockAdsAndTracking
       && isTPEnabled
@@ -424,7 +435,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 					request->url().host())
 				) {
 		block = true;
-    adsAndTrakersBlocked++;
+    adsAndTrackersBlocked++;
 	}
   bool isAdBlockEnabled = true;
   if (enable_ad_block_ && !shieldsSetExplicitly) {
@@ -432,6 +443,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
   }
 	const ResourceRequestInfo* info = ResourceRequestInfo::ForRequest(request);
 	if (!block
+      && isValidUrl
       && isGlobalBlockEnabled
       && blockAdsAndTracking
       && isAdBlockEnabled
@@ -442,7 +454,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 					request->url().spec(),
 					(unsigned int)info->GetResourceType())) {
 		block = true;
-    adsAndTrakersBlocked++;
+    adsAndTrackersBlocked++;
 	}
   bool check_httpse_redirect = true;
   if (block && content::RESOURCE_TYPE_IMAGE == info->GetResourceType()) {
@@ -453,6 +465,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 
   // HTTPSE work
   if (!block
+      && isValidUrl
       && isGlobalBlockEnabled
       && isHTTPSEEnabled
       && check_httpse_redirect
@@ -466,8 +479,8 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
     }
   }
   //
-  if (nullptr != shieldsConfig && (0 != adsAndTrakersBlocked || 0 != httpsUpgrades)) {
-    shieldsConfig->setBlockedCountInfo(last_first_party_url_.spec(), adsAndTrakersBlocked, httpsUpgrades, 0);
+  if (nullptr != shieldsConfig && (0 != adsAndTrackersBlocked || 0 != httpsUpgrades)) {
+    shieldsConfig->setBlockedCountInfo(last_first_party_url_.spec(), adsAndTrackersBlocked, httpsUpgrades, 0);
   }
 
   if (block && content::RESOURCE_TYPE_IMAGE != info->GetResourceType()) {
