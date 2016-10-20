93adca6131cd929f9dedb279a8e13ef6611d6db3
Serg <serg.zhukovsky@gmail.com>
2016-10-19 22:17:55 -0400

    Fixed a crash when we don't have a resource info
    Auditors: @bbondy

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -296,8 +296,9 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
         }
       }
   }
-  bool isValidUrl = request->url().is_valid();
+  bool isValidUrl = true;
   if (request) {
+      isValidUrl = request->url().is_valid();
       std::string scheme = request->url().scheme();
       if (scheme.length()) {
           std::transform(scheme.begin(), scheme.end(), scheme.begin(), ::tolower);
@@ -347,7 +348,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
     adsAndTrackersBlocked++;
 	}
   bool check_httpse_redirect = true;
-  if (block && content::RESOURCE_TYPE_IMAGE == info->GetResourceType()) {
+  if (block && info && content::RESOURCE_TYPE_IMAGE == info->GetResourceType()) {
     check_httpse_redirect = false;
     *new_url = GURL(TRANSPARENT1PXGIF);
   }
@@ -355,6 +356,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
 
   // HTTPSE work
   if (!block
+      && request
       && isValidUrl
       && isGlobalBlockEnabled
       && isHTTPSEEnabled
@@ -373,7 +375,7 @@ int ChromeNetworkDelegate::OnBeforeURLRequest(
     shieldsConfig->setBlockedCountInfo(last_first_party_url_.spec(), adsAndTrackersBlocked, httpsUpgrades, 0);
   }
 
-  if (block && content::RESOURCE_TYPE_IMAGE != info->GetResourceType()) {
+  if (block && (nullptr == info || content::RESOURCE_TYPE_IMAGE != info->GetResourceType())) {
 		*new_url = GURL("");
 
 		return net::ERR_BLOCKED_BY_ADMINISTRATOR;
