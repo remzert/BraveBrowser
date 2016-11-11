6a6ea209be75c23b7e5d50bdf6b836d77c2692d2
Serg <serg.zhukovsky@gmail.com>
2016-11-10 21:24:50 -0500

    Added stats calculation
    Auditors: @aekeus, @bbondy, @garvankeeley

---
+++ chrome/android/java/src/org/chromium/chrome/browser/init/StatsObject.java
@@ -0,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.init;
+
+public class StatsObject {
+    public long mMilliSeconds;
+    public int mMonth;
+    public int mYear;
+
+    public StatsObject() {
+        mMilliSeconds = 0;
+        mMonth = 0;
+        mYear = 0;
+    }
+}
