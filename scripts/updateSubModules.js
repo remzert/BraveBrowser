1601b0200b6f96952b9fc546e71deec1e10c8866
Serg <serg.zhukovsky@gmail.com>
2016-12-15 22:52:50 -0500

    Corrected the update sub modules script

--- scripts/updateSubModules.js
+++ scripts/updateSubModules.js
@@ -1,3 +1,7 @@
+git submodule update --init hashset-cpp
+git submodule update --init tracking-protection
+git submodule update --init bloom-filter-cpp
+git submodule update --init abp-filter-parser-cpp
 git submodule update hashset-cpp
 git submodule update tracking-protection
 git submodule update bloom-filter-cpp
