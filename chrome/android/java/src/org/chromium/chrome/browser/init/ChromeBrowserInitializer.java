8586d716e895c906f3402e5015059c69cdcf7be3
Serg <serg.zhukovsky@gmail.com>
2017-01-16 15:04:54 -0500

    Disabled regional ad block option when no lists
    Auditors: @bbondy

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeBrowserInitializer.java
@@ -38,6 +38,7 @@ import org.chromium.chrome.browser.FileProviderHelper;
 import org.chromium.chrome.browser.crash.MinidumpDirectoryObserver;
 import org.chromium.chrome.browser.device.DeviceClassManager;
 import org.chromium.chrome.browser.preferences.PrefServiceBridge;
+import org.chromium.chrome.browser.preferences.privacy.PrivacyPreferencesManager;
 import org.chromium.chrome.browser.services.GoogleServicesManager;
 import org.chromium.chrome.browser.tabmodel.document.DocumentTabModelImpl;
 import org.chromium.chrome.browser.webapps.ActivityAssigner;
@@ -199,10 +200,19 @@ public class ChromeBrowserInitializer {
                 return null;
             }
 
+            final boolean enableRegionalAdBlock = (0 != files.size());
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    PrivacyPreferencesManager.getInstance().setRegionalAdBlock(enableRegionalAdBlock);
+                    PrefServiceBridge.getInstance().setAdBlockRegionalEnabled(enableRegionalAdBlock);
+                }
+            });
+
             for (int i = 0; i < files.size(); i ++) {
                 if (!ADBlockUtils.CreateDownloadedFile(mContext, files.get(i) + ".dat",
                     verNumber, ADBlockUtils.ADBLOCK_REGIONAL_LOCALFILENAME_DOWNLOADED, i != 0) && 0 == i) {
-                      break;
+                        break;
                     }
             }
 
